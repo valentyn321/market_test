4873b4ad238ad814191baacc41c22a525eed2ec4
Valentyn <valentyncherkasov24@gmail.com>
2020-05-17 22:52:39 +0300

    Added simple login & logout views from django.contrib. Maded simple base.html and mockups for login, logout.

--- market/settings.py
+++ market/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #installed apps:
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'market.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
