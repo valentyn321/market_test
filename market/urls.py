f39b50761cd39309b1a1a17e0fc9322c56cd96f0
Valentyn <valentyncherkasov24@gmail.com>
2020-05-17 22:52:39 +0300

    Added simple login & logout views from django.contrib. Maded simple base.html and mockups for login, logout.

--- market/urls.py
+++ market/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('login/', LoginView.as_view(template_name="auth/login.html"), name="login"),
+    path('logout/', LogoutView.as_view(template_name="auth/logout.html"), name="logout"),
 ]
