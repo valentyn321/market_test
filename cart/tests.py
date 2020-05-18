ed8315cb6466f1d40a3ae9a6c37989df6276973a
Valentyn <valentyncherkasov24@gmail.com>
2020-05-18 20:15:24 +0300

    Update models with validators. Create test for view, which check if template is correct, if it contains important inputs. Created view for addig new products, represented by FormView.

--- cart/tests.py
+++ cart/tests.py
@@ -1,5 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+
 from cart.models import Product
+
 
 class TestModels(TestCase):
     def set_up(self):
@@ -10,8 +13,19 @@ class TestModels(TestCase):
         fields = Product._meta.fields
         required_f = []
         required_f_values = []
-        for field in fields:  # checks all required fileds
+        for field in fields:
             if not field.blank:
                 required_f.append(str(field))
         for field in required_f_values:
             assert(field in self.product._meta.fields)
+
+
+class TestViews(TestCase):
+    def test_productcreate(self):
+        """Here we check our view ProductCreate, because there is a lot of
+        custom code, not only Django-packed"""
+        client = Client()
+        response = client.get(reverse('create_product'))
+        self.assertTemplateUsed(response, 'cart/create_product.html')
+        self.assertContains(response, "id_title")
+        self.assertContains(response, "id_price")
--- market/settings.py
+++ market/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #installed apps:
     'crispy_forms',
     'cart',
 ]
